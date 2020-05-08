3cabc67cee9489213cd2d3f26d7e5e480d8231cf
Anto111a <gotvjanskii@gmail.com>
2020-05-09 01:34:04 +0300

    Updates

---
+++ precache-manifest.928ad8253fa9f53d5fe553d162ef987a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6270b0d6f8b1a12559758fe0714760a",
+    "url": "/exchange_rate/index.html"
+  },
+  {
+    "revision": "af6250692fbf287428de",
+    "url": "/exchange_rate/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4b9dc1df2f48191ae18c",
+    "url": "/exchange_rate/static/css/main.1181d2fe.chunk.css"
+  },
+  {
+    "revision": "af6250692fbf287428de",
+    "url": "/exchange_rate/static/js/2.5301dd37.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/exchange_rate/static/js/2.5301dd37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9dc1df2f48191ae18c",
+    "url": "/exchange_rate/static/js/main.4611cb50.chunk.js"
+  },
+  {
+    "revision": "6a4d13428dba82afaca1",
+    "url": "/exchange_rate/static/js/runtime-main.2a9e821e.js"
+  }
+]);
